c88d2c34d0b9dbc71c10a8b72b6ed818e33a66b4
lunasolis <lunasolis@users.noreply.github.com>
2019-11-10 20:08:28 -0500

    Tutorial 3

---
+++ Assets/Scripts/Destroy_By_Boundary.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_By_Boundary : MonoBehaviour
+{
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+}
